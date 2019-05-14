d2ad7f09650a901209e8a8b9513d7780c2d90318
Philip Patsch <philip.patsch@tweag.io>
2019-05-14 14:00:18 +0200

    Add a ping command and a daemon that accepts the ping
    First working communication over the socket. If a `Ping` message
    arrives, the daemon replies with an ack, the client exits, the daemon
    prints “ping” and exits as well.

--- src/cli.rs
+++ src/cli.rs
@@ -42,9 +42,27 @@ pub enum Command {
     #[structopt(name = "watch")]
     Watch,
 
+    /// TODO
+    #[structopt(name = "daemon")]
+    Daemon,
+
+    /// TODO remove
+    #[structopt(name = "ping")]
+    Ping(Ping),
+
     /// Upgrade Lorri
     #[structopt(name = "self-upgrade", alias = "self-update")]
     Upgrade(UpgradeTo),
+}
+
+// TODO remove
+#[derive(StructOpt, Debug)]
+/// Ping the daemon to start a build
+pub struct Ping {
+    // TODO
+    #[structopt(parse(from_os_str))]
+    /// The nix file to watch and build on changes.
+    pub nix_file: PathBuf,
 }
 
 /// A stub struct to represent how what we want to upgrade to.
